6b85ea76cad6805bb156b3224d96207c701a6af5
richard davy <richard_davy@hotmail.com>
2018-09-11 14:05:02 +0200

    Ändrade lite grym text

--- Det var en gång/Program.cs
+++ Det var en gång/Program.cs
@@ -10,12 +10,15 @@ namespace Det_var_en_gång
     {
         static void Main(string[] args)
         {
-            string namn = "";
+            Console.WriteLine("Vad älskar du mest?");
+            string ord = Console.ReadLine();
+            Console.WriteLine("Vad hatar du mest?");
+            string ord2 = Console.ReadLine();
+            Console.WriteLine("Vad heter du?");
+            string namn = Console.ReadLine();
             Console.WriteLine("Det var en grabb, han bodde i staden.");
             Console.WriteLine("Han var många år gammal och hans bästa vän hade ett namn");
             Console.WriteLine("Vännen " + namn +  " var lika många år gammal som grabben");
-            string ord = "hattar";
-            string ord2 = "mössor";
             
             Console.WriteLine("Han äskade " + ord + " och hatade " + ord2);
         }
